446d70216fc96e7a85e0aa560168d41c8551eede
Umherirrender <umherirrender_de.wp@web.de>
2020-06-01 11:24:26 +0200

    build: Bump phan to 0.10.2, remove taint-check
    Change-Id: I88fe3bf962aafa15eb1fb9fd9a11a41e73c1a358

--- includes/AnisaTemplate.php
+++ includes/AnisaTemplate.php
@@ -261,6 +261,7 @@ class AnisaTemplate extends BaseTemplate {
 					}
 					break;
 				default:
+					// @phan-suppress-next-line SecurityCheck-DoubleEscaped
 					$html .= $this->getPortlet( $name, $content['content'] );
 					break;
 			}
